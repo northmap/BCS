e724c86c38f724f6d6e9f354d96d0aef8c765ab2
northmap <92556115+northmap@users.noreply.github.com>
2025-03-16 11:36:13 +0000

    Update BCS_config.js

--- BCS_config.js
+++ BCS_config.js
@@ -2,7 +2,7 @@
 const config = {
     style: "mapbox://styles/jbamford/cl7ylpomb007715qotuwdm3gy",
     accessToken: "pk.eyJ1IjoiamJhbWZvcmQiLCJhIjoia0dZNENUMCJ9._cSAJxiTZhfdH6F1LhxnVw",
-    CSV: "https://docs.google.com/spreadsheets/d/1U6ZTiTxWvtS64N9XGaIW2uj9p_Hl7nhnAckcR-pJNmc/gviz/tq?tqx=out:csv&sheet=Sheet1",
+    CSV: "BCS.csv",
     center: [-6.438,55.233], //Lng, Lat
     zoom: 12.7, //Default zoom
     pitch: 5,
