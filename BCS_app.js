4a6b283c5aa8cce05ef895b66e04c36082c19035
northmap <92556115+northmap@users.noreply.github.com>
2025-03-16 12:38:36 +0000

    Update BCS_app.js

--- BCS_app.js
+++ BCS_app.js
@@ -7,8 +7,8 @@ const filteredGeojson = {
   features: [],
 };
 var bounds = [
-  [-6.75626,55.11992], // Southwest coordinates
-  [-6.14105,55.32863], // Northeast coordinates
+  [-6.33078, 55.18797], // Southwest coordinates
+  [-6.13943, 55.33703], // Northeast coordinates
 ];
 const map = new mapboxgl.Map({
   container: "map",
@@ -27,7 +27,7 @@ function flyToLocation(currentFeature, zoom) {
   map.flyTo({
     center: currentFeature,
     offset: [0, -150],
-    zoom: 15.4,
+    zoom: 15,
     pitch: 0,
     speed: 0.3, // make the flying slow
     curve: 0.9,
@@ -457,8 +457,8 @@ map.on("load", function () {
     csv2geojson.csv2geojson(
       csvData,
       {
-        latfield: "Lat",
-        lonfield: "Long",
+        latfield: "Latitude",
+        lonfield: "Longitude",
         delimiter: ",",
       },
       function (err, data) {
@@ -478,25 +478,41 @@ map.on("load", function () {
           paint: {
             "circle-radius": [
               "interpolate",
-              ["exponential", 0.51],
+              ["linear"],
               ["zoom"],
-              12,
+              12.5,
               3,
-              14.4,
-              5
+              15,
+              6.5,
             ], // size of circles
-            "circle-color": "hsl(2, 63%, 49%)", // color of circles
-            "circle-stroke-color": "hsl(0, 1%, 98%)",
+            "circle-color": [
+              "match",
+              ["get", "Theme"],
+              ["COAST"],
+              "hsl(196, 58%, 61%)",
+              ["Historical sites"],
+              "hsl(0, 83%, 64%)",
+              ["Visitor information"],
+              "hsl(288, 68%, 68%)",
+              ["Environmental"],
+              "hsl(141, 67%, 56%)",
+              ["Other"],
+              "hsl(232, 82%, 71%)",
+              ["Walking and cycling trails"],
+              "hsl(43, 62%, 60%)",
+              "#000000",
+            ], // color of circles
+            "circle-stroke-color": "hsl(298, 3%, 100%)",
             "circle-stroke-width": [
               "interpolate",
               ["linear"],
               ["zoom"],
-              12,
-              1,
-              14.4,
-              3
+              12.5,
+              1.5,
+              15,
+              3,
             ],
-            "circle-opacity": 1
+            "circle-opacity": 1,
           },
         });
 
@@ -508,15 +524,15 @@ map.on("load", function () {
             data: geojsonData,
           },
           layout: {
-            "text-field": "{Title/Name}",
+            "text-field": "{Title/name}",
             "text-size": [
               "interpolate",
               ["linear"],
               ["zoom"],
+              12.5,
               12,
-              12,
-              15.5,
-              16
+              15,
+              15,
             ],
             "text-offset": [
               "interpolate",
@@ -525,7 +541,7 @@ map.on("load", function () {
               12,
               ["literal", [0, 0.5]],
               15,
-              ["literal", [0, 1.3]]
+              ["literal", [0, 1.3]],
             ],
             "text-justify": "center",
             "text-padding": 0.5,
@@ -534,26 +550,34 @@ map.on("load", function () {
           },
           paint: {
             "text-opacity": 1,
-            "text-color": "hsl(2, 63%, 49%)",
+            "text-color": [
+              "match",
+              ["get", "Theme"],
+              ["COAST"],
+              "hsl(196, 24%, 35%)",
+              ["Historical sites"],
+              "hsl(0, 57%, 61%)",
+              ["Visitor information"],
+              "hsl(288, 32%, 51%)",
+              ["Environmental"],
+              "hsl(141, 31%, 48%)",
+              ["Other"],
+              "#5f6bbf",
+              ["Walking and cycling trails"],
+              "hsl(43, 26%, 31%)",
+              "#000000",
+            ],
             "text-halo-width": [
               "interpolate",
               ["linear"],
               ["zoom"],
-              12,
+              12.5,
               1,
               15,
-              1.5
+              2,
             ],
             "text-halo-color": "hsl(0, 100%, 99%)",
-            "text-halo-blur": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              12,
-              0,
-              15,
-              0.5
-            ],
+            "text-halo-blur": 0.2,
           },
         });
       }
@@ -632,4 +656,3 @@ $(document).on(
     }
   }
 );
-  
